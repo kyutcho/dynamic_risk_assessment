8c0bf9d0f72000e7982c1324c2df8a6e50594997
Jayden Cho - HP Omen <kyutcho@umich.edu>
2022-09-05 09:31:08 -0700

    added codes for fullprocess.py

--- fullprocess.py
+++ fullprocess.py
@@ -1,24 +1,49 @@
+import os
+import json
+import glob
 
+from ingestion import *
+from training import *
+from scoring import *
+from deployment import *
+from diagnostics import *
+from reporting import *
 
-import training
-import scoring
-import deployment
-import diagnostics
-import reporting
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+ingestion_file_path = os.path.join(config["prod_deployment_path"])
+data_csv_path = os.path.join(config["input_folder_path"])
 
 ##################Check and read new data
 #first, read ingestedfiles.txt
+def check_new_files():
+    curr_data_lst = []
+    with open(os.path.join(ingestion_file_path, "ingested_files.txt")) as f:
+        for line in f:
+            curr_data_lst.append(line.strip())
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+    # Check new data in input_folder_path
+    os.chdir(data_csv_path)
+    new_data_lst = glob.glob(f"*.csv")
+    os.chdir('..')
 
+    # Determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+    to_ingest_data_lst = set(new_data_lst).difference(set(curr_data_lst))
 
-
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
+    # If you found new data, you should proceed. otherwise, do end the process here
+    if len(to_ingest_data_lst) != 0:
+        merge_multiple_dataframe()
 
 
 ##################Checking for model drift
 #check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+with open(os.path.join(ingestion_file_path, 'latest_score.txt'), 'r') as f:
+    old_f1_score = float(f.read())
+
+train_model()
+
+new_f1_score = score_model(model, data)
 
 
 ##################Deciding whether to proceed, part 2
