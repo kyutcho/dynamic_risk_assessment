565b0f0049713ae30830ff543502e9c3bd1954de
Jayden Cho - MacBook Pro <kyutcho@umich.edu>
2022-09-05 11:17:06 -0700

    apicalls.py fixed

--- apicalls.py
+++ apicalls.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+URL = "http://127.0.0.1:8000"
 
 with open('config.json','r') as f:
     config = json.load(f) 
@@ -11,11 +11,13 @@ with open('config.json','r') as f:
 model_path = os.path.join(config['output_model_path'])
 test_data_path = os.path.join(config['test_data_path'])
 
+# print(f"{URL}/scoring")
+
 #Call each API endpoint and store the responses
 response1 = requests.post(url=f"{URL}/prediction", json={"data_name": os.path.join(test_data_path, "test_data.csv")}).content
-response2 = requests.post(url=f"{URL}/scoring").content
-response3 = requests.post(url=f"{URL}/summarystats").content
-response4 = requests.post(url=f"{URL}/diagnostics").content
+response2 = requests.get(url=f"{URL}/scoring").content
+response3 = requests.get(url=f"{URL}/summarystats").content
+response4 = requests.get(url=f"{URL}/diagnostics").content
 
 # #combine all API responses
 responses = f'''
@@ -28,5 +30,3 @@ responses = f'''
 #write the responses to your workspace
 with open(os.path.join(model_path, "api_returns.txt"), "w") as f:
     f.write(responses)
-
-
